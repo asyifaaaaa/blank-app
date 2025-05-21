c53d216c045bbc7bb79f107798d57ffcab07b53b
asyifaaaaa <asyifamutia964@gmail.com>
2025-05-21 14:05:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("syalalalfa")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image("IMG-20250519-WA0076.jpg", width=200)
